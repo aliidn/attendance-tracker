9728b57dad143c4557da9edf3e26eef7e5c2d715
Ali DN <alidelaramnikoo@gmail.com>
2026-03-27 19:13:51 +0100

    updated email sending logic.

--- attendance-tracker.py
+++ attendance-tracker.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
+# Functions
+def send_email(student_email, current_leaves, status):
+    if status == 'warning':
+        print(f'sending warning Email to {student_email}, there is only 1 more leave is left for you ')
+    if status == 'alert':
+        print(f'sending alert Email to {student_email} and staff, {student_email} has {current_leaves} leaves.')
+
+#Start of program
+
 df = pd.read_csv('Attendance.csv')
 df['mailid'] = df['Student ID'].apply(lambda x : f'student_{x}@gmail.com') #adding a mail id column to the table
 
@@ -22,12 +31,13 @@ while True:
         student_email = df.loc[df['Student ID'] == student_id, 'mailid'].values[0]
 
         if current_leaves == 2:
-            print(f'sending warning Email to {student_email} there is only 1 more leave is left for you ')
+            send_email(student_email,current_leaves,'warning')
         elif current_leaves >= 3:
-            print(f'sending alert Email to {student_email} and staff')
+            send_email(student_email,current_leaves,'alert')
 
     if int(input('another subject? 1 for Yes, 0 for No')) == 0:
          break
 
 
 df.to_csv('Attendance1.csv', index= False)
+print("\n✅ All updates saved to Attendance1.csv")
